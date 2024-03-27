b50f0cd2cfa8fcc061ae292013cca03210f6c7d8
Allison <21110@fraser.school.nz>
2024-03-28 12:34:52 +1300

    28.03.24

---
+++ price_comparera.py
@@ -0,0 +1,31 @@
+#
+print ("Wecome to the Price Comparison Tool "
+       "what do you want to compare.")
+
+
+while True:
+ instructions = input("Do you want the instructions? ")
+ 
+ if instructions == "yes":
+    print("In this programme for it to work you will have to put in all of the things "
+          "you want to compare can not all be put in at once they have to be spaced out "
+          "other wise it will not work properly basically. ")
+    
+ else:
+  break
+
+
+
+
+while True:
+
+    item = input("what is the first item you want to compare? ")
+    item2 = input("what is the second item you want to compare? ")
+    item3 = input("what is the third item you want to compare? ")
+    item4 = input("what is the forth item you want to compare? ") 
+    item5 = input("what it the fifth item you want to compare? ")    
+    item6 = input("what it the sixth item you want to compare? ")
+
+
+
+
