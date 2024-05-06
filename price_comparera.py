9d1653f8907ea6e723b3111ceefcbbca47cf5eae
Allison <21110@fraser.school.nz>
2024-05-06 14:53:49 +1200

    6/05/24

--- price_comparera.py
+++ price_comparera.py
@@ -10,7 +10,7 @@ while True:
     print("In this programme for it to work you will have to put in all of the things "
           "you want to compare can not all be put in at once they have to be spaced out "
           "other wise it will not work properly basically. ")
-    
+    break
  else:
   break
 
@@ -18,50 +18,40 @@ while True:
 
 #user in put for the pirce checker.
 while True:
+  all_prices = [ ]
+  item1 = input("what is the first item and price you want to compare? ")
+  item1 = list(map(int, input().split()))
 
-    item1 = input("what is the first item you want to compare? ")
-    price1 = int(input("what is the price: "))
-    
-    item2 = input("what is the second item you want to compare? ")
-    price2 = int(input("what is the price: "))
-    
-    item3 = input("what is the third item you want to compare? ")
-    price3 = int(input("what is the price: ")) 
+  all_prices.sort()
    
-    item4 = input("what is the forth item you want to compare? ") 
-    price4 = int(input("what is the price: "))
+  item2 = input("what is the second item and price you want to compare? ")
+  item2 = list(map(int, input().split()))
+
+  all_prices.sort() 
     
-    item5 = input("what it the fifth item you want to compare? ")    
-    price5 = int(input("what is the price: "))
+  item3 = input("what is the third item and price you want to compare? ")
+  item3 = list(map(int, input().split()))
+
+  all_prices.sort()
    
-    item6 = input("what it the sixth item you want to compare? ")
-    price6 = int(input("what is the price: "))
+  item4 = input("what is the forth item and price you want to compare? ") 
+  item4 = list(map(int, input().split()))
+
+  all_prices.sort()
     
-    break
+  item5 = input("what it the fifth item and price you want to compare? ")    
+  item5 = list(map(int, input().split()))
+
+  all_prices.sort()
+   
+  item6 = input("what it the sixth item and price you want to compare? ")
+  item6 = list(map(int, input().split()))
+
+  all_prices.sort()
+    
+  break
 # this code checks for the chepiest price.
-all_prices = [ ]
 
-all_items = [ ]
-
-all_prices.append(price1,price2,price3,price4,price5,price6)
-
-all_items.append(item1,item2,item3,item4,item5,item6)
-
-def price(e):
-  return e['lowest price']
-
-all_prices = [ ]
-
-all_prices.sort( reverse=True ,Key=price)
-
-print(all_prices)
-
-def price(e):
-  return e['lowest price']
-
-all_item = [ ]
-
-all_item.sort( reverse=True ,Key=price)
 
 print(all_prices)
 
