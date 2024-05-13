9562012d62760ad6fb3ab56e6c68e82bdb75ccad
Allison <21110@fraser.school.nz>
2024-05-13 14:51:25 +1200

    13/05/24

--- price_comparera.py
+++ price_comparera.py
@@ -4,8 +4,7 @@ print ("Wecome to the Price Comparison Tool "
 
 
 while True:
- instructions = input("Do you want the instructions? ")
- 
+ instructions = input("Do you want the instructions? ").lower
  if instructions == "yes":
     print("In this programme for it to work you will have to put in all of the things "
           "you want to compare can not all be put in at once they have to be spaced out "
@@ -15,7 +14,7 @@ while True:
     break
 
 else: 
-   print()
+   print("please ")
 
 
 #user in put for the pirce checker.
@@ -26,9 +25,7 @@ all_items = []
 
 all_prices = []
 
-for i in range(number):
-
-    
+for i in range(number): 
     item1 = input("what is the name of the item you want to compare? ")
     price1 = int(input("what is the price of the item you what to compare? "))
     all_prices.append(price1)
@@ -40,16 +37,16 @@ for i in range(number):
 # this code checks for the chepiest price.
 
 
-smallest_number = min(all_prices)
+price_number = min(all_prices)
 
 def smallest_number(all_prices):
-    smallest_number = all_prices[0]
+    price_number = all_prices[0]
     for item in all_prices:
-      if smallest_number > item:
-        smallest_number = item
-      return smallest_number
-
-print(smallest_number)
+      if price_number > item:
+        price_number = item
+      return price_number
+print(all_items)
+print(price_number)
 
 
 
